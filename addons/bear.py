5dd4fc0eddbea6005b947cdac315d7967d81d175
bendem <online@bendem.be>
2015-01-09 15:18:25 +0100

    [BEAR] Use a map so people can add more words to change

--- addons/bear.py
+++ addons/bear.py
@@ -1,9 +1,13 @@
-__module_name__ = "Bear PLEASE"
-__module_author__ = "bendem"
-__module_version__ = "1.0"
-__module_description__ = "say bear the right way"
+__module_name__        = "Bear"
+__module_author__      = "bendem"
+__module_version__     = "1.0"
+__module_description__ = "Say bear the right way"
 
 import hexchat
+
+words = {
+    'bear': 'ʕ•ᴥ•ʔ',
+}
 
 def bear(word, word_eol, userdata):
     if word[0] != "65293" or word[1] != "0": # 65293 is <enter> and 0 is no modifier
@@ -12,11 +16,16 @@ def bear(word, word_eol, userdata):
     if msg is None or len(msg) == 0 or msg[0] == '/':
         return
 
-    if 'bear' in msg:
-        msg = msg.replace('bear', 'ʕ•ᴥ•ʔ')
-        for user in hexchat.get_list('users'):
-            if 'bear' in user.nick:
-                msg = msg.replace(user.nick.replace('bear', 'ʕ•ᴥ•ʔ'), user.nick)
+    changed = False
+    for word, replace in words.items():
+        if word in msg:
+            msg = msg.replace(word, replace)
+            changed = True
+            for user in hexchat.get_list('users'):
+                if word in user.nick:
+                    msg = msg.replace(user.nick.replace(word, replace), user.nick)
+
+    if changed:
         hexchat.command("settext %s" % msg)
 
 hexchat.hook_print('Key Press', bear, priority = hexchat.PRI_LOWEST)
